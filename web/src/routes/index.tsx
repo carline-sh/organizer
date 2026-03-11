18a02aa384625e5ca2f0b5484707e4453178e6ec
carline <carline@v3x.email>
2026-03-11 16:28:38 +0100

    add home

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
@@ -6,6 +6,41 @@ export const Route = createFileRoute("/")({
 
 function RouteComponent() {
   return (
-    <div>welcome home</div>
+    <div className="border border-gray-300 rounded-md p-4">
+      <div className="">
+        <h1 className="text-xl text-cyan-700">This is organizer</h1>
+        <p>A tool to help you organize your life.</p>
+      </div>
+      <hr className="my-2 border-cyan-400" />
+      <div className="">
+        <h2 className="text-lg text-cyan-700">Features</h2>
+        <ul>
+          <li>
+            Add items to your
+            {" "}
+            <Link to="/groceries" className="link">groceries</Link>
+            ,
+          </li>
+          <li>
+            Keep track of items you have in
+            {" "}
+            <Link to="/stock" className="link">stock</Link>
+            ,
+          </li>
+          <li>
+            Save meals you like in
+            {" "}
+            <Link to="/recipes" className="link">recipes</Link>
+            ,
+          </li>
+          <li>
+            Plan your meals for the week in
+            {" "}
+            <Link to="/planning" className="link">planning</Link>
+            .
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
